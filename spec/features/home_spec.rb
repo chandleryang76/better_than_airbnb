3a711512fd22d151cb43670827cc829fdde5432e
Dan <chandleryang76@gmail.com>
2016-12-02 04:13:47 +1300

    Add Home page
    PT# 135327741

---
+++ spec/features/home_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+feature "visiting the home page" do
+  scenario "the visitor see the company name" do
+    visit root_path
+    expect(page).to have_text("Better than AirBnB")
+  end
+end
